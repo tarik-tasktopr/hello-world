0812115956463dab8d8f24d44b8fc9bab52ed252
TarikTaskToPR <tarik.tasktopr@gmail.com>
2023-04-15 20:49:22 +0200

    Edit /server.js

--- server.js
+++ server.js
@@ -1,1 +1,43 @@
-I am sorry, as an AI language model, I do not have access to the content of the file /server.js. Please provide me with the content so that I can assist you better.
+To serve the "index.html", "styles.css", and "script.js" files when requested by the browser, we need to modify the server.js file. We will use the fs module to read the contents of the requested file and send it as a response to the browser. Here is the updated code:
+
+```javascript
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './index.html';
+  }
+
+  const extname = String(path.extname(filePath)).toLowerCase();
+  let contentType = 'text/html';
+  switch (extname) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        res.writeHead(404);
+        res.end('Page not found');
+      } else {
+        res.writeHead(500);
+        res.end('Internal server error');
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
