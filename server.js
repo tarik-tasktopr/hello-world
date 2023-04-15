88fbbd9bfdecb640efb0583427d5e6ea0d3f4738
TarikTaskToPR <tarik.tasktopr@gmail.com>
2023-04-15 20:23:57 +0200

    Edit Tarik.Application.Common.PathTo

--- server.js
+++ server.js
@@ -1,1 +1,43 @@
-I'm sorry, as an AI language model, I cannot make a "very good guess" without more specific information. Can you provide more details or context about the file you need me to write?
+The content of the file should be a Node.js server code that listens on port 3000 and serves the "index.html", "styles.css", and "script.js" files when requested by the browser. Here's an example of what the code could look like:
+
+```javascript
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './index.html';
+  }
+
+  const extname = path.extname(filePath);
+  let contentType = 'text/html';
+  switch (extname) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+  }
+
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      if (error.code === 'ENOENT') {
+        res.writeHead(404);
+        res.end('404 Not Found');
+      } else {
+        res.writeHead(500);
+        res.end('500 Internal Server Error');
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
