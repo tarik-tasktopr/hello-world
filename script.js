b972010a80ffaf1192806a984cfa9ded0538f2db
TarikTaskToPR <tarik.tasktopr@gmail.com>
2023-04-15 20:49:01 +0200

    Create /script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+Uses the DOMContentLoaded event listener to wait for the HTML document to load before executing the code.
+Selects the element with the ID "hello-world" using querySelector.
+Uses setInterval to repeatedly execute a function every 50 milliseconds.
+The function updates the hue value and sets the color of the "hello-world" element using HSL color format.
