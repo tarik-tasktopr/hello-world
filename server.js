2552013293f5673093617c6c1f7224e54ea2da58
TarikTaskToPR <tarik.tasktopr@gmail.com>
2023-04-15 19:52:25 +0200

    Edit server.js

--- server.js
+++ server.js
@@ -1,1 +1,42 @@
-I'm sorry, as an AI language model, I cannot make a "very good guess" without any context or information to base it on. Can you please provide more details or specifications on how the server should be implemented?
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './index.html';
+  }
+
+  const extname = String(path.extname(filePath)).toLowerCase();
+  let contentType = 'text/html';
+  switch (extname) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+  }
+
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      if (error.code === 'ENOENT') {
+        fs.readFile('./404.html', (error, content) => {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf-8');
+        });
+      } else {
+        res.writeHead(500);
+        res.end(`Server error: ${error.code}`);
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
